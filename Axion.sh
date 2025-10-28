455a1ff2c325615d05907c264f84bb4817c3c5ad
gaurav-paul9 <paulgaurav9@gmail.com>
2025-10-29 00:30:30 +0530

    Dolby

--- Axion.sh
+++ Axion.sh
@@ -6,7 +6,7 @@ rm -rf prebuilts/clang/host/linux-x86
 
 git clone https://github.com/gaurav-paul9/android_device_oneplus_aston.git -b lineage-23.0 device/oneplus/aston --depth=1
 
-git clone https://github.com/LineageOS/android_device_oneplus_sm8550-common.git -b lineage-23.0 device/oneplus/sm8550-common --depth=1
+git clone https://github.com/gaurav-paul9/android_device_oneplus_sm8550-common.git -b lineage-23.0 device/oneplus/sm8550-common --depth=1
 
 git clone https://github.com/gaurav-paul9/android_kernel_oneplus_sm8550.git -b lineage-23.0 kernel/oneplus/sm8550 --depth=1
 
@@ -15,6 +15,8 @@ git clone https://github.com/LineageOS/android_kernel_oneplus_sm8550-modules.git
 git clone https://github.com/LineageOS/android_kernel_oneplus_sm8550-devicetrees.git  -b lineage-23.0 kernel/oneplus/sm8550-devicetrees --depth=1
 
 git clone https://github.com/LineageOS/android_hardware_oplus.git -b lineage-23.0 hardware/oplus --depth=1
+
+git clone https://github.com/inferno0230/hardware_dolby.git -b sixteen hardware/oplus --depth=1
 
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_aston.git -b lineage-23.0 vendor/oneplus/aston --depth=1
 
