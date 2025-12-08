5b7897512f4215b2c1894010e5f9dc9096a8f117
Gaurav Paul <154791745+gaurav-paul9@users.noreply.github.com>
2025-12-08 20:58:36 +0530

    Update infinityroot.sh

--- infinityroot.sh
+++ infinityroot.sh
@@ -73,6 +73,8 @@ rm -rf hardware/oplus
 rm -rf hardware/dolby
 rm -rf vendor/oneplus/aston
 rm -rf vendor/oneplus/sm8550-common
+rm -rf packages/apps/GameBar
+rm -rf .repo
 
 echo "Performing selective cleanup of 'out' directory..."
 rm -rf out/target/product/aston/system
