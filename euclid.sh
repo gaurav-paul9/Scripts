0aadf8dfb7dcba2356fab57d71c67b9cc025632a
Gaurav Paul <154791745+gaurav-paul9@users.noreply.github.com>
2025-12-12 08:24:19 +0530

    Update euclid.sh

--- euclid.sh
+++ euclid.sh
@@ -101,7 +101,7 @@ fi
 # =======================
 echo "Cloning additional repositories..."
 git clone https://github.com/gaurav-paul9/android_device_oneplus_aston-qpr1.git -b lineage-23.1 device/oneplus/aston --depth=1
-git clone https://github.com/OnePlus12R-development/android_device_oneplus_sm8550-common.git -b sixteen-qp1 device/oneplus/sm8550-common --depth=1
+git clone https://github.com/OnePlus12R-development/android_device_oneplus_sm8550-common.git -b sixteen-qpr1 device/oneplus/sm8550-common --depth=1
 git clone https://github.com/OnePlus12R-development/android_kernel_oneplus_sm8550.git -b sixteen-qpr1 kernel/oneplus/sm8550 --depth=1
 git clone https://github.com/OnePlus12R-development/android_kernel_oneplus_sm8550-modules.git -b sixteen-qpr1 kernel/oneplus/sm8550-modules --depth=1
 git clone https://github.com/OnePlus12R-development/android_kernel_oneplus_sm8550-devicetrees.git -b sixteen-qpr1 kernel/oneplus/sm8550-devicetrees --depth=1
