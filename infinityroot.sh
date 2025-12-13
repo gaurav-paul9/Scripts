eaeb1cb618b7d055e8ba9f4557af618d3e7a4500
Gaurav Paul <154791745+gaurav-paul9@users.noreply.github.com>
2025-12-13 11:01:38 +0530

    infclean in infinityroot.sh

--- infinityroot.sh
+++ infinityroot.sh
@@ -101,20 +101,14 @@ fi
 #   4. CLONING ADDITIONAL REPOSITORIES
 # =======================
 echo "Cloning additional repositories..."
-git clone https://github.com/gaurav-paul9/android_device_oneplus_aston.git -b inf device/oneplus/aston --depth=1
-git clone https://github.com/gaurav-paul9/android_device_oneplus_sm8550-common.git -b lineage-23.0 device/oneplus/sm8550-common --depth=1
+git clone https://github.com/gaurav-paul9/android_device_oneplus_aston.git -b infclean device/oneplus/aston --depth=1
+git clone https://github.com/gaurav-paul9/android_device_oneplus_sm8550-common.git -b infclean device/oneplus/sm8550-common --depth=1
 git clone https://github.com/gaurav-paul9/android_kernel_oneplus_sm8550.git -b newroot kernel/oneplus/sm8550 --depth=1
 git clone https://github.com/LineageOS/android_kernel_oneplus_sm8550-modules.git -b lineage-23.0 kernel/oneplus/sm8550-modules --depth=1
 git clone https://github.com/gaurav-paul9/android_kernel_oneplus_sm8550-devicetrees.git -b lineage-23.0 kernel/oneplus/sm8550-devicetrees --depth=1
 git clone https://github.com/LineageOS/android_hardware_oplus.git -b lineage-23.0 hardware/oplus --depth=1
-git clone https://github.com/inferno0230/hardware_dolby.git -b sixteen hardware/dolby --depth=1
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_aston.git -b lineage-23.0 vendor/oneplus/aston --depth=1
 git clone https://gitlab.com/NoPrincessHere/proprietary_vendor_oneplus_sm8550-common.git -b sixteen vendor/oneplus/sm8550-common --depth=1
-git clone https://github.com/gaurav-paul9/packages_apps_GameBar.git packages/apps/GameBar -b main
-git clone https://github.com/zen0s-aospforge/vendor_revanced.git vendor/revanced
-
-# Revanced Stuff
-bash vendor/revanced/extract-libs.sh
 
 # =======================
 #   6. BUILD THE ROM
